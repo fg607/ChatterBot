7aa24ccd781a083fbfa4c80d2c406dc1b44cb2a4
fg607 <gangf333@gmail.com>
2020-03-28 11:29:50 +0800

    添加str(text)

--- chatterbot/tagging.py
+++ chatterbot/tagging.py
@@ -223,13 +223,13 @@ class ChineseTagger(object):
         bigram_pairs = []
 
 	 #利用正则表达式去掉一些一些标点符号之类的符号。
-        text = re.sub(r'\s+', ' ', text)  # trans 多空格 to空格
-        text = re.sub(r'\n+', ' ', text)  # trans 换行 to空格
-        text = re.sub(r'\t+', ' ', text)  # trans Tab to空格
-        text = re.sub("[\s+\.\!\/_,$%^*(+\"\']+|[+——；！，”。《》，。：“？、~@#￥%……&*（）1234567①②③④)]+".\
-                          encode().decode("utf8"), "".encode().decode("utf8"), text)
+        text = re.sub(r'\s+', ' ', str(text))  # trans 多空格 to空格
+        text = re.sub(r'\n+', ' ', str(text))  # trans 换行 to空格
+        text = re.sub(r'\t+', ' ', str(text))  # trans Tab to空格
+        #text = re.sub("[\s+\.\!\/_,$%^*(+\"\']+|[+——；！，”。《》，。：“？、~@#￥%……&*（）1234567①②③④)]+".\
+        #                  encode().decode("utf8"), "".encode().decode("utf8"), text)
   
-        wordlist = list(jieba.cut(text))#jieba.cut  把字符串切割成词并添加至一个列表
+        wordlist = list(jieba.cut(str(text)))#jieba.cut  把字符串切割成词并添加至一个列表
         for word in wordlist:
             if word not in self.stopword:#词语的清洗：去停用词
                 if word != '\r\n'  and word!=' ' and word != '\u3000'.encode().decode('unicode_escape') \
